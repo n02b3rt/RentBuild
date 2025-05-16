3e658eaa81369236c1ef8f9812e185002b94c457
Norbert Fxx <93492117+n02b3rt@users.noreply.github.com>
2025-05-16 13:39:02 +0200

    [fix] build

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container py-5">
+        <h1 class="text-3xl font-bold mb-4">Strona Główna</h1>
+        <p>Witaj w mojej aplikacji Laravel!</p>
+    </div>
+@endsection
