a21e134188185fac8c2a75d3bc66992f4fd5ae59
Norbert Fxx <93492117+n02b3rt@users.noreply.github.com>
2025-05-27 03:08:04 +0200

    [Feat] Created admin Equipment create view
    - Form for creating new Equipment entries
    - Includes fields for promotion and image upload
    - Styled using Tailwind CSS

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,5 +4,8 @@
     <section class="">
         <h1 class="text-2xl font-bold">Panel administratora</h1>
         <p class="mt-2 text-gray-600">Witaj w panelu admina!</p>
+        <a href="{{ route('admin.equipment.index') }}">Sprzęt (lista)</a>
+        <a href="{{ route('admin.equipment.create') }}" class="btn btn-primary mb-3">Dodaj nowy sprzęt</a>
+
     </section>
 @endsection
