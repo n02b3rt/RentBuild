644d5555217fb3fc27fe2987c3322b7c13edbe36
Krzysiu Majka <90238148+PLDrago@users.noreply.github.com>
2025-05-29 19:47:36 +0200

    [Feat] Implement email verification using server log mail driver

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,16 +3,21 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Fortify\Contracts\VerifyEmailViewResponse;
+use App\Http\Responses\VerifyEmailViewResponse as CustomVerifyEmailViewResponse;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    public function register(): void
+
+
+    public function register()
     {
-        //
+        $this->app->singleton(VerifyEmailViewResponse::class, CustomVerifyEmailViewResponse::class);
     }
+
 
     /**
      * Bootstrap any application services.
