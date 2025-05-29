ce691d4266cacb9dc42ff0346a41cc0e17cb55ce
Krzysiu Majka <90238148+PLDrago@users.noreply.github.com>
2025-05-29 18:52:40 +0200

    [Feat] Add user softdelete

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes; // dodaj import SoftDeletes
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, SoftDeletes; // dodaj SoftDeletes do traitów
 
     protected $fillable = [
         'first_name',
@@ -36,11 +37,6 @@ class User extends Authenticatable implements MustVerifyEmail
         'email_verified_at' => 'datetime',
         'account_balance' => 'decimal:2',
     ];
-
-//    public function sendEmailVerificationNotification()
-//    {
-//        $this->notify(new CustomVerifyEmail());
-//    }
 
     public function rentals()
     {
